49a1e5daae605852f501b50ada9cb3d9d161ce25
Kevin-Solomon <kevinsolomon404@gmail.com>
2022-05-01 21:26:50 +0530

    feat : add tabs for bookmark and posts

--- src/pages/private/Profile/Profile.jsx
+++ src/pages/private/Profile/Profile.jsx
@@ -1,7 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
-import { Avatar, Box, Text, Divider } from '@chakra-ui/react';
+import {
+  Avatar,
+  Box,
+  Text,
+  Divider,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+} from '@chakra-ui/react';
 import Navbar from '../../../components/Navbar/Navbar';
 function Profile() {
   const [user, setUser] = useState({
@@ -29,23 +39,44 @@ function Profile() {
   return (
     <Box>
       <Navbar />
-      <Box padding="0.8rem" margin="4rem auto" maxW="800px" d="flex" gap="3rem">
-        <Avatar size="2xl" />
-        <Box d="flex" gap="1rem" flexDirection="column">
-          <Text fontSize="2xl">{user.username}</Text>
-          <Box d="flex" gap="1rem">
-            <Box as="span" d="flex" gap="3px">
-              <Text fontWeight="900">{user.followers.length}</Text>followers
+      <Box padding="0.8rem" maxW="800px" margin="4rem auto">
+        <Box d="flex" gap="3rem">
+          <Avatar size="2xl" />
+          <Box d="flex" gap="1rem" flexDirection="column">
+            <Text fontSize="2xl">{user.username}</Text>
+            <Box d="flex" gap="1rem">
+              <Box as="span" d="flex" gap="3px">
+                <Text fontWeight="900">{user.followers.length}</Text>followers
+              </Box>
+              <Box as="span" d="flex" gap="3px">
+                <Text fontWeight="700">{user.following.length} </Text>following
+              </Box>
             </Box>
-            <Box as="span" d="flex" gap="3px">
-              <Text fontWeight="900">{user.following.length} </Text>following
-            </Box>
+            <Text>{`${user.firstName} ${user.lastName}`}</Text>
+            <Text>{user.bio}</Text>
           </Box>
-          <Text>{`${user.firstName} ${user.lastName}`}</Text>
-          <Text>{user.bio}</Text>
         </Box>
+        <Divider marginTop="10px" color="black" />
+        <Tabs isFitted variant="enclosed" marginTop="1rem">
+          <TabList>
+            <Tab>Posts</Tab>
+            <Tab>Bookmark</Tab>
+            <Tab>Tagged</Tab>
+          </TabList>
+
+          <TabPanels>
+            <TabPanel>
+              <p>one!</p>
+            </TabPanel>
+            <TabPanel>
+              <p>two!</p>
+            </TabPanel>
+            <TabPanel>
+              <p>three!</p>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
       </Box>
-      <Divider />
     </Box>
   );
 }
