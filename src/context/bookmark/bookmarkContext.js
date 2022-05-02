8ffad832d55a90506189ef05ce85d19fa048846f
Kevin-Solomon <kevinsolomon404@gmail.com>
2022-05-03 00:52:37 +0530

    feat : add initial get function

--- src/context/bookmark/bookmarkContext.js
+++ src/context/bookmark/bookmarkContext.js
@@ -1,9 +1,23 @@
 import { createContext, useContext, useReducer, useEffect } from 'react';
+import axios from 'axios';
 import { bookmarkReducer } from '../../reducer';
+import { useAuth } from '../auth/authContext';
 const BookmarkContext = createContext();
 
 const BookmarkProvider = ({ children }) => {
+  const { authState } = useAuth();
   const [bookmarkState, bookmarkDispatch] = useReducer(bookmarkReducer, []);
+  useEffect(() => {
+    async function getBookmarks() {
+      const response = await axios({
+        method: 'GET',
+        url: '/api/users/bookmark',
+        headers: { authorization: token },
+      });
+      bookmarkDispatch({ type: 'INITIAL', payload: response.data.bookmarks });
+    }
+    getBookmarks();
+  }, [authState.token]);
   return (
     <BookmarkContext.Provider value={{ bookmarkState, bookmarkDispatch }}>
       {children}
